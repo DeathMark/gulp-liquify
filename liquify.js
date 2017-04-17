f4d257383b2d23ba1a16cd136314b7a02f4529a2
Tuan Pham <tuanpm@skylinesoft.vn>
2017-04-17 16:46:49 +0700

    multiple include base

--- liquify.js
+++ liquify.js
@@ -3,71 +3,81 @@ var Promise = require("bluebird");
 var tinyliquid = require("tinyliquid");
 var path = require("path");
 
-var liquify = function(contents, locals, includeBase, prefix, filters){
-  var template;
-  var context = tinyliquid.newContext({
-      locals: locals,
-      filters: filters
-    });
+var liquify = function (contents, locals, includeBase, prefix, filters) {
+	var template;
+	var context = tinyliquid.newContext({
+		locals: locals,
+		filters: filters
+	});
 
-  if(!contents) {
-    contents = '';
-  };
+	if (!contents) {
+		contents = '';
+	}
+	;
 
-  if(typeof contents != "string"){
-    template = contents;
-  } else {
-    template = tinyliquid.compile(contents);
-  }
+	if (typeof contents != "string") {
+		template = contents;
+	} else {
+		template = tinyliquid.compile(contents);
+	}
 
-  return new Promise(function (resolve, reject) {
+	return new Promise(function (resolve, reject) {
 
-    context.onInclude(function (name, callback) {
+		context.onInclude(function (name, callback) {
 
-      var absolute = isAbsolute(name);
-      var ext = path.extname(name);
-      var filePath;
+			var absolute = isAbsolute(name);
+			var ext = path.extname(name);
+			var filePath;
 
-      if(!absolute) {
+			if (!absolute) {
 
-        if(prefix) {
-          name = prefix + name;
-        }
+				if (prefix) {
+					name = prefix + name;
+				}
 
-        if(!ext) {
-          name += ".liquid";
-        }
+				if (!ext) {
+					name += ".liquid";
+				}
 
-        filePath = path.join(includeBase, name);
+				if (Array.isArray(includeBase)) {
+					for (var i in includeBase) {
+						filePath	= path.join(includeBase[i], name);
 
-      } else {
-        filePath = name;
-      }
+						if (fs.existsSync(filePath)) {
+							break;
+						}
+					}
+				} else {
+					filePath = path.join(includeBase, name);
+				}
+			} else {
+				filePath = name;
+			}
 
 
-      fs.readFile(filePath, 'utf8', function (err, text) {
+			fs.readFile(filePath, 'utf8', function (err, text) {
 
-        if (err) {
-          reject(err);
-          return callback(err);
-        }
+				if (err) {
+					reject(err);
+					return callback(err);
+				}
 
-        var ast = tinyliquid.parse(text);
-        callback(null, ast);
-      });
-    });
+				var ast = tinyliquid.parse(text);
+				callback(null, ast);
+			});
+		});
 
-    template(context, function (err) {
-      if (err) return reject(err);
-      resolve(context.getBuffer(), template);
-    });
-  });
+		template(context, function (err) {
+			if (err) return reject(err);
+			resolve(context.getBuffer(), template);
+		});
+	});
 
 };
 
 function isAbsolute(p) {
-    if(path.isAbsolute) return path.isAbsolute(p);
-    return path.normalize(p + '/') === path.normalize(path.resolve(p) + '/');
+	if (path.isAbsolute) return path.isAbsolute(p);
+	return path.normalize(p + '/') === path.normalize(path.resolve(p) + '/');
 }
 
 module.exports = liquify;
